84258bab87c48146f1162797c02a2ba5800ca93b
Tom Dunlap <apocryphalauthor@gmail.com>
2014-11-25 12:10:11 -0500

    Add uzbl shortcut as mod+u

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -259,6 +259,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey,           }, "u",      function () awful.util.spawn("uzbl-browser") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
