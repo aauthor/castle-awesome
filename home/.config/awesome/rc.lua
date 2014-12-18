7e47457e2d8b57ed4d02ad84c4d46205fc779576
Tom Dunlap <apocryphalauthor@gmail.com>
2014-12-17 19:57:17 -0500

    Change mod key to alt to prevent collisions Mac host

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -50,7 +50,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+modkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
@@ -285,7 +285,7 @@ globalkeys = awful.util.table.join(
                   awful.util.getdir("cache") .. "/history_eval")
               end),
     awful.key({ modkey }, "p", function() menubar.show() end),
-    awful.key({ modkey }, "F2",    function ()
+    awful.key({ modkey, "Shift" }, "F2",    function ()
       awful.prompt.run({ prompt = "Rename tab: ", text = awful.tag.selected().name, },
       mypromptbox[mouse.screen].widget,
       function (s)
